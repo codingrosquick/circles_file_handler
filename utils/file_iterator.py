c4b257aa593553079b9ad3edbd062c45800b505e
Noé Carras <noecarras@MacBook-Pro-de-Noe.local>
2022-04-22 20:29:01 -0700

    starting file iterator

---
+++ utils/file_iterator.py
@@ -0,0 +1,191 @@
+from typing import Dict, List
+import pandas as pd
+from circles_file_handler.utils.cache import get_all_files_from_cache_dir
+from circles_file_handler.utils.cyverse_io_irods import IRODSGet
+from utils import init_cache, findall_files
+from global_variables import cyverse_path_server_resources_default, local_long_folder, local_temp_folder, cyverse_path_server_resources_user
+
+class FileServerCanGps:
+    """
+    Class handling file download and caching, csv filtering and time cuts
+    Works for retrieving files using CAN+GPS CSV data (from CyVerse runs)
+    """
+    # ============================== ATTRIBUTES ==============================
+
+    data = None
+    current_remote_adresses = None
+    current_event = None
+    can_local_address = None
+    gps_local_address = None
+    index = None
+    max_index = None
+    previous_cut_time = None
+    next_cut_time = None
+
+
+    # ============================== METHODS ==============================
+
+    def __init__(self):
+        self.index = 0
+
+
+    def __str__(self):
+        if self.max_index is None:
+            return f'FileServer configuration is not finished'
+        else:
+            return f'file server with {self.max_index} files ready to be served, current index is: {self.index}'
+
+
+    # -------------- Getting and setting which exploration to use --------------
+
+    def set_exploration_to_use(self, local_exploration_to_use: str):
+        '''
+        Sets which exploration (available locally) to use
+        :param local_exploration_to_use: path to the exploration CSV to use
+        '''
+        self.data = pd.read_csv(local_exploration_to_use)
+        self.max_index = len(self.data)
+
+
+    async def get_default_explorations(self, clear_long_cache: bool = False) -> None:
+        '''
+        Gets from CyVerse the default explorations of CSV CAN+GPS path couples
+
+        :param clear_long_cache: Set to true to clear the cache before downloading those default explorations
+        :return: void
+        '''
+        if clear_long_cache:
+            init_cache(long=True)
+
+        files = await findall_files(root=cyverse_path_server_resources_default)
+
+        for file in files:
+            await IRODSGet(remote_address=file, cache_address=local_long_folder)
+
+   
+    async def find_available_user_explorations(self) -> List[str]:
+        '''
+        Find the path to all the user explorations.
+        Useful to call the method get_specific_user_exploration() and use a specific user defined exploration path.
+
+        :return: List of remote path (on CyVerse) of the available user explorations
+        '''
+        files = await findall_files(self, root=cyverse_path_server_resources_user)
+        return files
+
+
+    def find_locally_available_explorations(self) -> List[str]:
+        '''
+        Gives the path to all the local explorations, already downloaded locally.
+        Use this to select the right exploration to use for your file-to-file analysis.
+        
+        '''
+        files = get_all_files_from_cache_dir(long=True)
+        return files
+
+
+    async def get_specific_user_exploration(self, remote_path: str, clear_long_cache: bool = False) -> None:
+        '''
+        Gets from CyVerse the default explorations of CSV CAN+GPS path couples
+        :param remote_path: path to CyVerse user exploration
+        :param clear_long_cache: Set to true to clear the cache before downloading those default explorations
+        :return: void
+        '''
+        if clear_long_cache:
+            init_cache(long=True)
+
+        await IRODSGet(remote_address=remote_path, cache_address=local_long_folder)
+
+
+    # -------------- Filtering --------------
+
+    def filter(self, cc_state: List[int] = None, speed: Dict[str, int] = None, vin: List[str] = None, date: Dict[str, str] = None, event_type: List[str] = None):
+        """
+        filter the rows based on those criteria
+        :param cc_state: list of acceptable controller state values
+        :param speed: {min: int min_speed in km/h, max: int max_speed in km/h}
+        :param vin: list of acceptable vehicle identification numbers
+        :param date: {beg: date, end: date}, with date as strings, formatted as YYYY-MM-DD-HH-MM-SS
+        :param event_type: list of acceptable event types. possible event types are:
+            - car_crossing
+            - <more to come in the future>
+        :return: updates self.data to only keep the desirable instances
+        """
+        if event_type is not None:
+            self.data = self.data.loc[self.data['event_type'] in event_type]
+        if vin is not None:
+            self.data = self.data.loc[self.data['vin'] in vin]
+        if cc_state is not None:
+            self.data = self.data.loc[self.data['event_cc_state'] in cc_state]
+        if speed is not None:
+            self.data = self.data.loc[(self.data['event_speeds'] >= speed['min'])
+                                  & (self.data['event_speeds'] >= speed['min'])]
+        if date is not None:
+            self.data = self.data.loc[(self.data['date'] >= date['min'])
+                                  & (self.data['date'] >= date['min'])]
+        self.max_index = len(self.data)
+
+
+    # -------------- Cutting --------------
+
+    def set_cut(self, previous, next):
+        """
+        sets the values of the parameters of the cut for the csv CAN/GPS files
+        :param previous: number of seconds kept before the event
+        :param next: number of seconds kept after the event
+        :return: void. updates the attributes of the file server
+        """
+        self.previous_cut_time = previous
+        self.next_cut_time = next
+
+    def cut_can_file(self):
+        """
+        cut the current file according to the cutting parameters defined
+        """
+        event_time = self.current_event['event_time']
+        can_new_path = perform_cut(self.can_local_address, self.previous_cut_time, self.next_cut_time, event_time)
+        gps_new_path = None
+        if self.gps_local_address is not None:
+            gps_new_path = perform_cut(self.can_local_address, self.previous_cut_time, self.next_cut_time, event_time)
+        return {
+            'can': can_new_path,
+            'gps': gps_new_path,
+        }
+
+
+    # -------------- Retrieving file through next() method --------------
+
+    async def next(self, ignore_gps_file: bool = False):
+        """
+        clears cache & downloads the next couple of files
+        :param: ignore_gps_file: set to True to avoid downloading the GPS file
+        :return: - object with paths to the downloaded CAN and GPS file
+        {'can': str, 'gps': str, 'remote_addresses': {'can': str, 'gps': str}}
+                 - if the maximum index is reached, returns an exception as:
+        Exception('max_index')
+        """
+        try:
+            print(f'serving and preprocessing file, number {self.index} out of {self.max_index}')
+            if self.index < self.max_index:
+                cache = init_cache(self.local_root_folder)
+                self.current_event = self.data.iloc[self.index]
+                self.current_remote_adresses = ast.literal_eval(self.current_event['remote_addresses'])
+
+                self.can_local_address = await iget(self.current_remote_adresses['can'], cache)
+                if ignore_gps_file:
+                    self.gps_local_address = None
+                else:
+                    self.gps_local_address = await iget(self.current_remote_adresses['gps'], cache)
+
+                self.index += 1
+
+                local_addresses = self.cut_can_file()
+                print(f'Download and preprocessing of {local_addresses} was successful')
+                return local_addresses
+            else:
+                raise Exception('max_index')
+
+        except Exception as e:
+            raise Exception(f'Downloading next file failed on {e}')
+
+

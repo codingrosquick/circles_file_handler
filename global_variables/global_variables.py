ba82e49ec7bb778106da0de0c1bdf7b7eb14c771
Noé Carras <noecarras@MacBook-Pro-de-Noe.local>
2022-04-26 15:59:44 -0700

    new files and preparation for pip publishing. Script for configuration made in __main__ python file to call

--- global_variables/global_variables.py
+++ global_variables/global_variables.py
@@ -1,15 +1,12 @@
-# CYVERSE PATH
 import os
 
+# CYVERSE PATH
 cyverse_path_server_resources = '/iplant/home/noecarras/resources_file_iterator/'
 cyverse_path_server_resources_default = '/iplant/home/noecarras/resources_file_iterator/default/'
 cyverse_path_server_resources_user = '/iplant/home/noecarras/resources_file_iterator/user/'
 cyverse_path_server_resources_test = '/iplant/home/noecarras/resources_file_iterator/test/'
 
 # LOCAL CACHES
-# NOTE: use absolute path to avoid issues
-# TODO: remove my personal path when publishing
-# local_folder = '<path-to-your-project>'
 local_folder = '/Users/noecarras/Documents/03_Berkeley_EECS/cours/Capstone_RL_validation/filehandler/circles_file_handler'
 
 local_temp_folder = os.path.join(local_folder, 'temp_cache')
